fca6c5c394b148c8cb1e5b7d1665321c1045362b
Alexsageauthor <alexsageauthor@gmail.com>
2026-09-06 20:26:55 +1200

    Add files via upload

--- portraits.js
+++ portraits.js
@@ -64,12 +64,12 @@ const PORTRAITS = {
     "V_The_Reception": "bundle-V_The_Reception-129dd958be.webp"
   },
   "gold": {
-    "III_The_Invitation": "bundle-III_The_Invitation-3c500293b8.webp",
-    "II_The_Planning": "bundle-II_The_Planning-1102af2dae.webp",
+    "III_The_Invitation": "bundle-III_The_Invitation-2df1ced47e.webp",
+    "II_The_Planning": "bundle-II_The_Planning-f863aa5de3.webp",
     "IV_The_Ceremony": "bundle-IV_The_Ceremony-1fd16b1841.webp",
-    "I_The_Announcement": "bundle-I_The_Announcement-26f4d057d2.webp",
-    "Ia_Your_Wedding_Online": "bundle-Ia_Your_Wedding_Online-5735f517c4.webp",
-    "VI_The_Complete_Collection": "bundle-VI_The_Complete_Collection-6965635b13.webp",
+    "I_The_Announcement": "bundle-I_The_Announcement-6fdb60b5ae.webp",
+    "Ia_Your_Wedding_Online": "bundle-Ia_Your_Wedding_Online-10a1f82bc0.webp",
+    "VI_The_Complete_Collection": "bundle-VI_The_Complete_Collection-b5b4ae2d6c.webp",
     "VI_With_Thanks": "bundle-VI_With_Thanks-9e9672c965.webp",
     "V_The_Reception": "bundle-V_The_Reception-570dd811bc.webp"
   },
